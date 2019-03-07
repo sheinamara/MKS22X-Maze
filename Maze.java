d9a2ef5f248f0a7076124992d396b2e6d5610cc4
sheinamara <ashein00@stuy.edu>
2019-03-07 11:21:20 -0500

    working on solve

--- Maze.java
+++ Maze.java
@@ -60,6 +60,69 @@ public class Maze{
     }
   }
 
+  public void setAnimate(boolean b){
+    animate = b;
+  }
+
+  public void clearTerminal(){
+    System.out.println("\033[2J\033[1;1H");
+  }
+
+  public String toString(){
+    String aMazing = "";
+    for (int x = 0; x < maze.length; x++){
+      aMazing = aMazing + "\n";
+      for (int y = 0; y < maze[x].length; x++){
+        aMazing = aMazing + maze[x][y];
+      }
+    }
+    return aMazing;
+  }
+
+
+
+  //find the location of the S.
+
+
+  //erase the S
+
+
+  //and start solving at the location of the s.
+
+  //return solve(???,???);
+  public int solve(){
+    return 1;
+  }
+
+  /*
+    Recursive Solve function:
+
+    A solved maze has a path marked with '@' from S to E.
+
+    Returns the number of @ symbols from S to E when the maze is solved,
+    Returns -1 when the maze has no solution.
+
+
+    Postcondition:
+
+      The S is replaced with '@' but the 'E' is not.
+
+      All visited spots that were not part of the solution are changed to '.'
+
+      All visited spots that are part of the solution are changed to '@'
+  */
+  private int solve(int row, int col){ //you can add more parameters since this is private
+    if (animate){
+      clearTerminal();
+      System.out.println(this);
+      wait(20);
+    }
+
+      //COMPLETE SOLVE
+
+      return -1; //so it compiles
+  }
+
     /*Constructor loads a maze text file, and sets animate to false by default.
 
       1. The file contains a rectangular ascii maze, made with the following 4 characters:
@@ -79,36 +142,12 @@ public class Maze{
 
 
 
-
-
-
-     public void setAnimate(boolean b){
-        animate = b;
-     }
-
-    public void clearTerminal(){
-
-        //erase terminal, go to top left of screen.
-
-        System.out.println("\033[2J\033[1;1H");
-
-    }
-
     /*Return the string that represents the maze.
 
      It should look like the text file with some characters replaced.
 
     */
-    public String toString(){
-      String aMazing = "";
-      for (int x = 0; x < maze.length; x++){
-        aMazing = aMazing + "\n";
-        for (int y = 0; y < maze[x].length; x++){
-          aMazing = aMazing + maze[x][y];
-        }
-      }
-      return aMazing;
-    }
+
 
     /*Wrapper Solve Function returns the helper function
 
@@ -116,53 +155,6 @@ public class Maze{
       Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
 
     */
-    public int solve(){
-
-            //find the location of the S.
 
 
-            //erase the S
-
-
-            //and start solving at the location of the s.
-
-            //return solve(???,???);
-
-            return 1;
-
-    }
-
-    /*
-      Recursive Solve function:
-
-      A solved maze has a path marked with '@' from S to E.
-
-      Returns the number of @ symbols from S to E when the maze is solved,
-      Returns -1 when the maze has no solution.
-
-
-      Postcondition:
-
-        The S is replaced with '@' but the 'E' is not.
-
-        All visited spots that were not part of the solution are changed to '.'
-
-        All visited spots that are part of the solution are changed to '@'
-    */
-    private int solve(int row, int col){ //you can add more parameters since this is private
-
-
-        //automatic animation! You are welcome.
-        if(animate){
-
-            clearTerminal();
-            System.out.println(this);
-
-            wait(20);
-        }
-
-        //COMPLETE SOLVE
-
-        return -1; //so it compiles
-    }
 }
